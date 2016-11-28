20b5c175f9e6a9b5ca3758f6ceb787f2aeceeb18
TetleyAce <j.williams2907@gmail.com>
2016-11-28 11:13:30 +0000

    AppFiles

---
+++ script.js
@@ -0,0 +1,16 @@
+function play1(){
+    var audio1 = document.getElementById("audio1");
+    audio1.play();
+}
+function play2(){
+    var audio2 = document.getElementById("audio2");
+    audio2.play();
+}
+function play3(){
+    var audio3 = document.getElementById("audio3");
+    audio3.play();
+}
+function play4(){
+    var audio4 = document.getElementById("audio4");
+    audio4.play();
+}
