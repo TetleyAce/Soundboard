d2b57ce58a92935d6bb95bc5b7d2b039e0504d68
TetleyAce <j.williams2907@gmail.com>
2016-11-30 17:05:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,67 @@ function play4(){
     var audio4 = document.getElementById("audio4");
     audio4.play();
 }
+var inAppBrowserRef;
+ 
+function showRequest() {
+ 
+    var target = "_blank";
+ 
+    var options = "location=yes,hidden=yes";
+ 
+    inAppBrowserRef = cordova.InAppBrowser.open('http://oliverleigh.co.uk/sur/new.php', target, options);
+ 
+    inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
+ 
+    inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
+ 
+    inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
+ 
+}
+ 
+function loadStartCallBack() {
+ 
+    $('#status-message').text("loading please wait ...");
+ 
+}
+ 
+function loadStopCallBack() {
+ 
+    if (inAppBrowserRef != undefined) {
+ 
+        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;" });
+ 
+        $('#status-message').text("");
+ 
+        inAppBrowserRef.show();
+    }
+ 
+}
+ 
+function loadErrorCallBack(params) {
+ 
+    $('#status-message').text("");
+ 
+    var scriptErrorMesssage =
+       "alert('Sorry we cannot open that page. Message from the server is : "
+       + params.message + "');"
+ 
+    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
+ 
+    inAppBrowserRef.close();
+ 
+    inAppBrowserRef = undefined;
+ 
+}
+ 
+function executeScriptCallBack(params) {
+ 
+    if (params[0] == null) {
+ 
+        $('#status-message').text(
+           "Sorry we couldn't open that page. Message from the server is : '"
+           + params.message + "'");
+    }
+ 
+}
+ 
